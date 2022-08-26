ff10ec896d85e23797352798639f4a87c292a4c1
saras singh <saras.k.singh@gmail.com>
2022-08-26 14:23:18 +0530

    comment change

--- spring-boot-junit/src/test/java/com/javatrainingschool/ExampleServiceTest.java
+++ spring-boot-junit/src/test/java/com/javatrainingschool/ExampleServiceTest.java
@@ -7,6 +7,7 @@ import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
+//this is a test class
 @SpringBootTest
 public class ExampleServiceTest {
 
